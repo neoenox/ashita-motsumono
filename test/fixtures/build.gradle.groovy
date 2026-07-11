b44e5a2c42783e024eb8bd327173c7291e1895b0
kaenozu <106388957+kaenozu@users.noreply.github.com>
2026-07-11 20:53:41 +0900

    test: add Groovy Android build fixture

---
+++ test/fixtures/build.gradle.groovy
@@ -0,0 +1,21 @@
+plugins {
+    id 'com.android.application'
+}
+
+android {
+    compileSdkVersion 34
+
+    defaultConfig {
+        applicationId 'com.example.fixture'
+        minSdkVersion 21
+        targetSdkVersion 34
+    }
+
+    compileOptions {
+        sourceCompatibility JavaVersion.VERSION_11
+        targetCompatibility JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+}
