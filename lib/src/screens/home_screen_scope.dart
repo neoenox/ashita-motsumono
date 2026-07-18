8441fd518c8a35685a273327c2100e89fb404d37
kaenozu <106388957+kaenozu@users.noreply.github.com>
2026-07-18 17:35:26 +0900

    fix: restrict lost image recovery to real Android

--- lib/src/screens/home_screen_scope.dart
+++ lib/src/screens/home_screen_scope.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
+import 'dart:io' show Platform;
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -28,7 +28,7 @@ class _HomeScreenScopeState extends State<HomeScreenScope> {
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
-    if (_recoveryStarted || defaultTargetPlatform != TargetPlatform.android) {
+    if (_recoveryStarted || !Platform.isAndroid) {
       return;
     }
     _recoveryStarted = true;
