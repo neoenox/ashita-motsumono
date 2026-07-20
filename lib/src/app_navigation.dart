4da3f5bc343485230b532945e21a6c941a5d4132
kaenozu <106388957+kaenozu@users.noreply.github.com>
2026-07-20 12:19:03 +0900

    feat: add adaptive navigation helper

---
+++ lib/src/app_navigation.dart
@@ -0,0 +1,45 @@
+// lib/src/app_navigation.dart
+// プラットフォームごとの標準操作感を保った画面遷移を提供する。
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+/// 現在のプラットフォームに適したページルートを生成する。
+///
+/// [Navigator.pushReplacement] のスタック動作を維持する箇所でも同じ判定を
+/// 再利用できるよう、ルート生成を分離している。
+PageRoute<T> adaptivePageRoute<T>(
+  WidgetBuilder builder, {
+  bool fullscreenDialog = false,
+}) {
+  switch (defaultTargetPlatform) {
+    case TargetPlatform.iOS:
+    case TargetPlatform.macOS:
+      return CupertinoPageRoute<T>(
+        builder: builder,
+        fullscreenDialog: fullscreenDialog,
+      );
+    case TargetPlatform.android:
+    case TargetPlatform.fuchsia:
+    case TargetPlatform.linux:
+    case TargetPlatform.windows:
+      return MaterialPageRoute<T>(
+        builder: builder,
+        fullscreenDialog: fullscreenDialog,
+      );
+  }
+}
+
+Future<T?> pushAdaptive<T>(
+  BuildContext context,
+  WidgetBuilder builder, {
+  bool fullscreenDialog = false,
+}) {
+  return Navigator.of(context).push<T>(
+    adaptivePageRoute<T>(
+      builder,
+      fullscreenDialog: fullscreenDialog,
+    ),
+  );
+}
