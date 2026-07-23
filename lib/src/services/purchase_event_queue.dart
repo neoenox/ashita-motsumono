65123e96052b77e339abd0b1dc55fec78871cafb
kaenozu <106388957+kaenozu@users.noreply.github.com>
2026-07-23 16:39:19 +0900

    refactor: make purchase event queue type safe

--- lib/src/services/purchase_event_queue.dart
+++ lib/src/services/purchase_event_queue.dart
@@ -4,11 +4,25 @@ import 'dart:async';
 class PurchaseEventQueue {
   Future<void> _tail = Future<void>.value();
 
-  Future<T> enqueue<T>(FutureOr<T> Function() action) {
+  Future<void> enqueue(FutureOr<void> Function() action) {
+    final result = Completer<void>();
+    _tail = _tail.then((_) async {
+      try {
+        await action();
+        result.complete();
+      } on Object catch (error, stackTrace) {
+        result.completeError(error, stackTrace);
+      }
+    });
+    return result.future;
+  }
+
+  Future<T> enqueueValue<T>(FutureOr<T> Function() action) {
     final result = Completer<T>();
     _tail = _tail.then((_) async {
       try {
-        result.complete(await action());
+        final value = await action();
+        result.complete(value);
       } on Object catch (error, stackTrace) {
         result.completeError(error, stackTrace);
       }
