60a5468a93b778c0e1d0f5bc284744f04f42509c
kaenozu <106388957+kaenozu@users.noreply.github.com>
2026-08-05 11:34:07 +0900

    style: format deadline extraction

--- lib/src/services/date_extractor.dart
+++ lib/src/services/date_extractor.dart
@@ -18,9 +18,7 @@ class DateExtractor {
   static final _ambiguousDeadlinePattern = RegExp(
     r'(今月末|月末|始業式の日|終業式の日|入学式の日|卒園式の日|卒業式の日|運動会の日|遠足の日)',
   );
-  static final _deadlineKeywordPattern = RegExp(
-    r'(提出期限|提出日|持参日|締切|期限|まで)',
-  );
+  static final _deadlineKeywordPattern = RegExp(r'(提出期限|提出日|持参日|締切|期限|まで)');
 
   static const _weekdayMap = <String, int>{
     '月': DateTime.monday,
